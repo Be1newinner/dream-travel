adb71edcabc089e91db1dc16a8588042f3d78d92
Tahir Saifi <tahirsaifi413@gmail.com>
2026-03-05 17:23:51 +0530

    feat: Add a new home page featuring a tabbed hero search component for various travel options.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Hero from "@/components/home/Hero";
+import HeroSearch from "@/components/home/HeroSearch";
 
 export default function Home() {
   return (
     <>
       <Hero />
-
+      <HeroSearch />
     </>
   );
 }
