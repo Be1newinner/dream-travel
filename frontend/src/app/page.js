8a2a3a62a240d4a23e9b54a56520432f49e337a2
Tahir Saifi <tahirsaifi413@gmail.com>
2026-03-06 16:00:53 +0530

    Implement initial home page structure, including layout, hero, search, and offers components with new assets.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "@/components/home/Hero";
 import HeroSearch from "@/components/home/HeroSearch";
+import Offer from "@/components/home/Offers";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <HeroSearch />
+      <Offer />
     </>
   );
 }
